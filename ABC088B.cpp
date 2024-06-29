5a784b0518322429af853bf4d3511e46f888d82b
tatuki kosikawa <159102831+tatu-coding@users.noreply.github.com>
2024-06-29 15:43:28 +0900

    6/29

---
+++ ABC088B.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    int a[num];
+    int b;
+
+    for (int i = 0; i < num; i++) {
+        cin >> a[i];
+    }
+
+    bool boolean;
+    boolean = true;
+
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = i + 1; j <  num - i - 1; j++) {
+            if (a[i] < a[j]) {
+                b = a[i];
+                a[i] = a[j];
+                a[j] = b;
+            }
+        }
+    }
+
+    int sum1 = 0;
+    int sum2 = 0;
+
+    for (int i = 0; i < num; i++) {
+        if (i%2 == 0) {
+            sum1 += a[i];
+        } else {
+            sum2 += a[i];
+        }
+    }
+
+    if (sum1 > sum2) {
+        cout << sum1 - sum2 << endl;
+    } else {
+        cout << sum2 - sum1 << endl;
+    }
+}
